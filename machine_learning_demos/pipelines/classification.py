a4a36273b454d998be1a74b3fc249acd7e786d54
David Ashton <ashtondavi@gmail.com>
2022-08-12 16:36:06 +1200

    Added argparse demo code to classification pipeline
    Added argparse demo code to classification pipeline. Argparse is being used to parse command line arguements to the classification pipeline.

--- machine_learning_demos/pipelines/classification.py
+++ machine_learning_demos/pipelines/classification.py
@@ -1,10 +1,52 @@
+import argparse
 import numpy
 
 from ..modules import models
+
 
 class Classification():
     """Placeholder class for classification pipeline
     """
 
     def __init__(self):
+        self.epochs = 10
         variables = numpy.array([])
+
+    def set_model(self) -> None:
+        """Set the model architecture
+        """
+
+        self.model = models.ModelX()
+
+    def set_args(self, args) -> None:
+        """Sets the class variables based on input args
+        """
+
+        self.epochs = args.epochs
+
+    def run(self) -> None:
+        """Run the pipeline
+
+        Args:
+            epochs (int, optional): _description_. Defaults to 10.
+        """
+
+        self.set_model()
+
+
+def main():
+    """Main function to parse command line arguements and run pipeline
+    """    
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', dest="epochs", type=int, help='Sets the number of epochs')
+    args = parser.parse_args()
+
+    pipeline = Classification()
+    pipeline.set_args(args)
+    pipeline.run()
+    Classification.run()
+
+
+if __name__ == "__main__":
+    main()
